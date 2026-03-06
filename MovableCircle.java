f2a571da0dff5eab6fc292802d5eee16d8e4dfe3
TobyAD123 <tad218@exeter.ac.uk>
2026-03-06 13:26:44 +0000

    movablecircle.java done

--- MovableCircle.java
+++ MovableCircle.java
@@ -1,14 +1,42 @@
 
-public class MovableCircle implements Movable { 
+public class MovableCircle implements Movable {
   private MovablePoint center;
   private int radius;
   
-  // TASK: Add the constructor here
+  public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius) {
+    this.radius = radius;
+    this.center = new MovablePoint(x, y, xSpeed, ySpeed);
+
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder stringObj = new StringBuilder();
+    stringObj.append("MovableCircle[center=MovablePoint[" + this.center.toString() + "]");
+    stringObj.append(",radius=" + this.radius);
+
+    return stringObj.toString();
+  }
   
   // TASK: Implement all abstract methods declared in the interface
   @Override
   public void moveUp() {
 	 center.moveUp();// move the center up
+  }
+
+  @Override
+  public void moveDown() {
+    center.moveDown();
+  }
+
+  @Override
+  public void moveLeft() {
+    center.moveLeft();
+  }
+
+  @Override
+  public void moveRight() {
+    center.moveRight();
   }
 
 }
