9f7b61eed856779815f7c8a5f0aec5a942086b65
TobyAD123 <tad218@exeter.ac.uk>
2026-03-06 13:09:28 +0000

    movablepoint.java done

--- MovablePoint.java
+++ MovablePoint.java
@@ -4,6 +4,23 @@ public class MovablePoint implements Movable {
 	
 	// TASK: Add the constructor below
 
+	public MovablePoint(int x, int y, int xSpeed, int ySpeed) {
+		this.x = x;
+		this.y = y;
+		this.xSpeed = xSpeed;
+		this.ySpeed = ySpeed;
+	}
+
+	public String toString() {
+		StringBuilder stringObj = new StringBuilder();
+		stringObj.append("x="+ this.x +",");
+		stringObj.append("y="+ this.y +",");
+		stringObj.append("xSpeed="+ this.xSpeed +",");
+		stringObj.append("ySpeed="+ this.ySpeed +",");
+
+		return stringObj.toString();
+	}
+
 
 
 	// Implement abstract methods declared in the interface
@@ -13,6 +30,21 @@ public class MovablePoint implements Movable {
 	}
 
 	// TASK: Code the other interface methods below
+
+	public void moveDown() {
+		y -= ySpeed;
+	}
+
+	public void moveLeft() {
+		x += xSpeed;
+	}
+
+	public void moveRight() {
+		x -= xSpeed;
+	}
+
+
+	
 
 
 }
